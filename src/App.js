d7e8d0602beabaf30c5608679689672fd40d7778
caveman49 <caveman4949@gmail.com>
2021-11-19 17:02:08 -0500

    not good

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { Component } from "react";
+import productData from "./data/productData";
+// import formatPrice from "./helpers/formatPrice";
 import "./App.css";
 
-const App = () => {
-  return <h1>Hello, world!</h1>;
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cart: [],
+      checkout: productData.name,
+    };
+  }
+
+  render() {
+    return (
+      <body>
+        <h1>Mod 3 Mid</h1>
+        <productData className="products" />
+        <form onSubmit={this.handleFormSubmit} id="checkout">Form</form>
+      </body>
+    );
+  }
+}
 
 export default App;
